2171cbf4387989984acf719fdd3b62cb555b7392
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-04-09 13:05:53 +0530

    config for test added

--- config/config.js
+++ config/config.js
@@ -4,20 +4,23 @@ module.exports = {
     password: null,
     database: process.env.DB_NAME || "rcu",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "postgres" 
+    dialect: "postgres",
+    port : 5432
   },
   test: {
     username: process.env.DB_USER || "postgres",
     password: null,
     database: process.env.DB_NAME || "rcu",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "postgres" 
+    dialect: "postgres",
+    port : 2345 
   },
   production: {
     username: "postgres",
     passwor: null,
     database: "rcu",
     host: "127.0.0.1",
-    dialect: "postgres"
+    dialect: "postgres",
+    port : 5432 
   }
 }
